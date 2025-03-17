353bccb6577eaa4ca3d2cc68dafcbc09513e38b5
HalDaiva <daivahalilintar@gmail.com>
2025-03-17 21:10:14 +0700

    Create guru pamong relation
    add guru_id attribute

---
+++ database/migrations/2025_03_17_135656_update_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('guru_id')->nullable(); // Allow users without a referrer
+            $table->foreign('guru_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
