d56d7ac9dcfc6694d9f7e19d681eae3d33acca45
farisfian06 <farisfian06@gmail.com>
2025-03-18 12:22:15 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredGuruPamongContoller;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\LogbookController;
 use Illuminate\Http\Request;
@@ -10,13 +11,16 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
 
+Route::group(['prefix' => 'guru-pamong'], function () {
+    Route::post('/register', [RegisteredGuruPamongContoller::class, 'store']);
+});
+
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('/user', function (Request $request) {
-        return  response()->json($request->user());
+    Route::group(['prefix' => 'guru-pamong'], function () {
+        Route::put('/logbook/validasi/{id}', [LogbookController::class, 'updateStatus'])->name('logbooks.updateStatus');
     });
 
     Route::get('/logbooks', [LogbookController::class, 'index'])->middleware('role:Kaprodi,Mahasiswa');
-    Route::post('/logbooks', [LogbookController::class, 'store']);
+    Route::post('/logbooks', [LogbookController::class, 'store'])->name('logbooks.store');
 });
-
