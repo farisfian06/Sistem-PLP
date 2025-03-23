176c299505cbff672b6cc2d78899663cdab4dcb4
farisfian06 <farisfian06@gmail.com>
2025-03-23 22:15:04 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => $request->role,
+            'role' => $request->role ?? 'Mahasiswa',
             'details' => json_encode($request->details)
         ]);
 
