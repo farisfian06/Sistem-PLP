25405094a0709fd4211b53bc150f741f56beb7a0
Ilavf <ila990474@gmail.com>
2025-03-26 14:39:37 +0700

    chore: integrate Flowbite React for UI components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbiteReact from "flowbite-react/plugin/tailwindcss";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.tsx',
+        ".flowbite-react\\class-list.json"
     ],
 
     theme: {
@@ -18,5 +21,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, flowbiteReact],
 };
